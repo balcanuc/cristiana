874fb7b272ad12d27c5f237cf34305c41b380ece
Cristian Balcanu <balcanu@amazon.com>
2019-01-29 20:16:59 -0800

    accesibil: fix esteaccesibil, add devineaccesibil
    accesibil: fix esteaccesibil, add devineaccesibil

--- accesibil/main.cpp
+++ accesibil/main.cpp
@@ -1,22 +1,66 @@
 #include <bits/stdc++.h>
 
 using namespace std;
+
 ifstream fin ("accesibil.in");
 ofstream fout ("accesibil.out");
-int v[100003], w[25000];
+int v[100003];
+int w[5];
 
 int esteaccesibil(int n)
 {
-    int b = 0;
+    // un numar este accesibl daca are cel putin 2 cifre sau altfel spus este >= 10
+    if(n < 10)
+        return 0;
+
+    int c = n % 10; // este ultima cifra
+    n = n / 10; // n este numarul n din care tot taiam cate o cifra. 
+    // pentru ca mergem de la cap la coada, cifrele care au ramas 
+    // trebuie sa fie in ordine descrescatoare
     while ( n > 0 )
     {
-        int c = n % 10;
-        if ( c == b )
+        if( n % 10 != (c - 1) ) 
+        {
             return 0;
-        b = c;
+        }
+        c = n % 10;
         n = n / 10;
     }
     return 1;
+}
+
+int nrcifre(int n) 
+{
+    int r = 0;
+    while(n != 0) 
+    {
+        n = n / 10;
+        r++;
+    }
+    return r;
+}
+
+
+int devineaccesibil(int n) 
+{
+    if(esteaccesibil(n))
+        return 0;
+    int nrcifre_n = nrcifre(n);
+    int p10 = 1; // puterea lui 10 ce corespunde cifrei pe care vrem sa o eliminam
+    while(true) 
+    {
+        int m1 = n % p10; // parta de dupa cifra pe care vrem sa o eliminam
+        int m2 = n / (p10 * 10); // partea de dinaintea cifrei pe care o eliminam
+        int m = m1 + m2 * p10; // construim numarul 
+        if(esteaccesibil(m) && nrcifre(m) == nrcifre_n - 1)
+            return 1;
+        p10 = p10 * 10;
+        if(m2 == 0)
+            return 0; // iesim daca partea de dinainte este 0
+        // cu alte cuvinte la iteratia curenta am eliminat  prima cifra
+
+    }        
+
 }
 
 int pow10(int n)
@@ -29,32 +73,65 @@ int pow10(int n)
     return p;
 }
 
+int mai_mic(int a, int b) 
+{
+    if(a > b)
+        return 1;
+    return 0;
+}
+
 int main()
 {
     int c, k, n, j = 0;
+
+    // cout << "esteaccesibil(123) = " << esteaccesibil(123) << endl;
+    // cout << "esteaccesibil(1235) = " << esteaccesibil(1235) << endl;
+    // cout << "esteaccesibil(1) = " << esteaccesibil(1) << endl;
+    // cout << "esteaccesibil(234) = " << esteaccesibil(234) << endl;
+    // cout << "esteaccesibil(32756) = " << esteaccesibil(32756) << endl;
+
+    // cout << "devineaccesibil(123) = " << devineaccesibil(123) << endl;
+    // cout << "devineaccesibil(1233) = " << devineaccesibil(1233) << endl;
+    // cout << "devineaccesibil(12333) = " << devineaccesibil(12333) << endl;
+    // cout << "devineaccesibil(2123456789) = " << devineaccesibil(2123456789) << endl;
+
+
     fin >> c;
     fin >> k >> n;
     for ( int i = 1 ; i <= n ; i++ )
         fin >> v[i];
     if ( c == 1 )
     {
+        j = 1;
         for ( int i = 1 ; i <= n ; i++ )
         {
             if ( esteaccesibil(v[i]) == 1 )
             {
+                // tinem maxim 4 elemente in w
+                // adaugam in ultima pozitie
+                // le sortam de fiecare data in ordine descrescatore
+                // la sfarsit vor fi pe primele 3 pozitii cele mai mari numere.
+
                 w[j] = v[i];
-                j++;
+                if(j < 4)
+                {
+                    j++;
+                }
+                sort(w + 1, w + 1 + j, mai_mic);
             }
         }
-        sort (w+1, w+1+j);
-        for ( int i = j - 2  ; i <= j; i++  )
+        if(j > 3)
+            j = 3;
+        sort(w + 1, w + 1 + j);
+        for ( int i = 1  ; i <= j; i++  )
             fout << w[i] << " ";
     }
     else if ( c == 2 )
     {
-        int c = 0;
+        int c = 0; // nu este bine sa folosesti aceasi variabila ca sa nu te incurci. 
         for ( int i = 1 ; i <= n ; i++ )
         {
+            if(devineaccesibil(v[i]))
             c++;
         }
         fout << c;
