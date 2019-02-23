33407c4622ab568463aec69c0a73f2c182c25d4b
EC2 Default User <ec2-user@ip-172-31-45-164.eu-west-1.compute.internal>
2019-02-23 21:41:51 +0000

    fix Cristian's bug

--- submit/submit.cpp
+++ submit/submit.cpp
@@ -3,10 +3,14 @@
 using namespace std;
 ifstream fin ("submit.in");
 ofstream fout ("submit.out");
-int v[103][103], w[103];
+int v[103][103]; // solutii
+int w[103]; // punctaje
 int main()
 {
-    int n, m, p, smax = 0;
+    int smax = 0;
+    int n; // nr teste
+    int m; // nr de 
+    int p; // bonus 
     fin >> n;
     for ( int i = 1 ; i <= n ; i++ )
         fin >> w[i];
@@ -14,15 +18,20 @@ int main()
     for ( int i = 1 ; i <= m ; i++ )
         for ( int j = 1 ; j <= n ; j++ )
             fin >> v[i][j];
-    int a = 0;
+    int a = 0; // penalizare
     for ( int i = 1 ; i <= m ; i++ )
     {
-        int s = p;
+        int s = 0; 
+        bool corect = true; // true daca toate sunt corecte, false altfel
         for ( int j = 1 ; j <= n ; j++ )
         {
             if ( v[i][j] == 1 )
                 s = s + w[j];
+            else 
+                corect = false;
         }
+        if(corect)
+            s = p; 
         s = s - a;
         a = a + 2;
         if ( s > smax )
