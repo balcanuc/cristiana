7a9f1c03e005141cc8ec0f0df00bb2e882e932ad
EC2 Default User <ec2-user@ip-172-31-45-164.eu-west-1.compute.internal>
2019-02-23 22:00:48 +0000

    do not use an array for v, use a single integer

--- submit/submit.cpp
+++ submit/submit.cpp
@@ -3,7 +3,6 @@
 using namespace std;
 ifstream fin ("submit.in");
 ofstream fout ("submit.out");
-int v[103][103]; // solutii
 int w[103]; // punctaje
 int main()
 {
@@ -15,17 +14,17 @@ int main()
     for ( int i = 1 ; i <= n ; i++ )
         fin >> w[i];
     fin >> p >> m;
-    for ( int i = 1 ; i <= m ; i++ )
-        for ( int j = 1 ; j <= n ; j++ )
-            fin >> v[i][j];
     int a = 0; // penalizare
     for ( int i = 1 ; i <= m ; i++ )
     {
+
         int s = 0; 
         bool corect = true; // true daca toate sunt corecte, false altfel
         for ( int j = 1 ; j <= n ; j++ )
         {
-            if ( v[i][j] == 1 )
+            int v;
+            fin >> v;
+            if ( v == 1 )
                 s = s + w[j];
             else 
                 corect = false;
